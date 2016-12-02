9fd2602b9ab12476552e3be455ba7bc9d3662729
dikey.huang <dikey.huang@chroma.com.tw>
2016-12-02 08:38:50 +0800

    c++ library add -lstdc++ parameter.

--- make_link_a.sh
+++ make_link_a.sh
@@ -2,3 +2,5 @@
 rm ./libtest.a
 ./make_a.sh
 gcc -static main.c -L. -ltest -o test_a
+#c++ add -lstdc++
+#gcc main.c -L. -ltest -o test_so -lstdc++
--- make_link_so.sh
+++ make_link_so.sh
@@ -2,4 +2,6 @@
 rm ./libtest.so
 ./make_so.sh
 gcc main.c -L. -ltest -o test_so
+#c++ add -lstdc++
+#gcc main.c -L. -ltest -o test_so -lstdc++
 
