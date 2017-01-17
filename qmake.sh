a9d3e9d3552f1a3781637f53cb2b5fc6c2fee452
dikey.huang <dikey.huang@chroma.com.tw>
2017-01-17 15:13:38 +0800

    modified qmake.sh : use QTemplate.pro.

--- qmake.sh
+++ qmake.sh
@@ -1,68 +1,67 @@
 #!/bin/bash
 
-QPRO_NAME="QComm_Msg.pro"
+
+echo "====qmake.sh============================================="
+
+if [ $1 = "-h" ]; then
+    echo "qmake.sh -project (Just once)"
+    echo "qmake.sh -d -app" 
+    echo "qmake.sh -d -libshared"
+    echo "qmake.sh -d -libstatic"
+    echo "qmake.sh -r -app"
+    echo "qmake.sh -r -libshared"
+    echo "qmake.sh -r -libstatic"
+    exit 0
+fi 
 
 DIR_TOP=$(cd `dirname $0` ; pwd)
-echo ${DIR_TOP}
+BASENAME=$(basename  $DIR_TOP)
+PRO_NAME=$BASENAME.pro
+QPRO_NAME="Q"$PRO_NAME
 
+echo "DIR_TOP="${DIR_TOP}
+echo "BASENAME="${BASENAME}
+echo "PRO_NAME="${PRO_NAME}
+echo "QPRO_NAME="${QPRO_NAME}
+
+#######################################################################################
+# qmake -project
+#######################################################################################
+if [ $1 = "-p" ]; then
+    qmake -project
+    cp QTemplate.pro $QPRO_NAME
+    exit 0
+fi 
+
+#######################################################################################
+# &1 = debug or release
+# &2 = -app or -libshared or -libstatic
+#######################################################################################
 if [ $1 = "-d" ]; then
     echo "debug build.."
     unset QMAKESPEC  
     echo "QMAKESPEC=" $QMAKESPEC
-    QDEFINE=$QMAKESPEC
+    DEFS=x86$2
 elif [ $1 = "-r" ]; then
     echo "relrese build.."   
     cd /sitara/ti-sdk-am335x-evm/linux-devkit/
     . ./environment-setup
     unset QMAKESPEC  
     echo "QMAKESPEC=" $QMAKESPEC
-    QDEFINE=$QMAKESPEC
+    DEFS=arm$2
 else
     echo "error build.."
     exit -1
 fi 
 cd ${DIR_TOP}
 
-qmake -project
 #######################################################################################
-# app_Comm_Msg.pro to Makefile
+# generate Makefile and make
 #######################################################################################
-#copy Comm_Msg.pro the DEPENDPATH, INCLUDEPATH, HEADERS, SOURCES to QPRO_NAME
-if [ $2 = "-app" ]; then
-    echo "app Makefile."
-    if [ $1 = "-d" ]; then
-    	qmake QDEFS=x86_app -o Makefile $QPRO_NAME
-    elif [ $1 = "-r" ]; then
- 	qmake QDEFS=arm_app -o Makefile $QPRO_NAME
-    fi
-    time make
-fi
-#######################################################################################
-# libso_Comm_Msg.pro to Makefile
-#######################################################################################
-#copy Comm_Msg.pro the DEPENDPATH, INCLUDEPATH, HEADERS, SOURCES to QPRO_NAME 
-if [ $2 = "-libshared" ]; then
-    echo "shared library Makefile."
-    if [ $1 = "-d" ]; then
-    	qmake QDEFS=x86_sharedlib -o Makefile QPRO_NAME
-    elif [ $1 = "-r" ]; then
- 	qmake QDEFS=arm_sharedlib -o Makefile QPRO_NAME
-    fi
-    time make
-fi
-#######################################################################################
-# liba_Comm_Msg.pro to Makefile
-#######################################################################################
-#copy Comm_Msg.pro the DEPENDPATH, INCLUDEPATH, HEADERS, SOURCES to QPRO_NAME
-if [ $2 = "-libstatic" ]; then
-    echo "static library Makefile."
-    if [ $1 = "-d" ]; then
-    	qmake QDEFS=x86_staticlib -o Makefile QPRO_NAME
-    elif [ $1 = "-r" ]; then
- 	qmake QDEFS=arm_staticlib -o Makefile QPRO_NAME
-    fi
-    time make
-fi
+qmake QDEFS=$DEFS QTARGET_NAME=$BASENAME -o Makefile $QPRO_NAME
+time make
+exit 0
+
 
 
 
