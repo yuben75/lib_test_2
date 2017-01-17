9488b2f9492c6048b350b7ab54046929e1a66083
dikey.huang <dikey.huang@chroma.com.tw>
2017-01-17 12:42:05 +0800

    modified QComm_Msg.pro and qmake.sh.

--- qmake.sh
+++ qmake.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-
+QPRO_NAME="QComm_Msg.pro"
 
 DIR_TOP=$(cd `dirname $0` ; pwd)
 echo ${DIR_TOP}
-
 
 if [ $1 = "-d" ]; then
     echo "debug build.."
@@ -22,51 +21,45 @@ else
     echo "error build.."
     exit -1
 fi 
-
-echo
-
 cd ${DIR_TOP}
 
-
+qmake -project
 #######################################################################################
 # app_Comm_Msg.pro to Makefile
 #######################################################################################
-qmake -project
-#copy Comm_Msg.pro the DEPENDPATH, INCLUDEPATH, HEADERS, SOURCES to QComm_Msg.pro  
+#copy Comm_Msg.pro the DEPENDPATH, INCLUDEPATH, HEADERS, SOURCES to QPRO_NAME
 if [ $2 = "-app" ]; then
     echo "app Makefile."
     if [ $1 = "-d" ]; then
-    	qmake QDEFS=x86_app -o Makefile QComm_Msg.pro
+    	qmake QDEFS=x86_app -o Makefile $QPRO_NAME
     elif [ $1 = "-r" ]; then
- 	qmake QDEFS=arm_app -o Makefile QComm_Msg.pro
+ 	qmake QDEFS=arm_app -o Makefile $QPRO_NAME
     fi
     time make
 fi
 #######################################################################################
 # libso_Comm_Msg.pro to Makefile
 #######################################################################################
-qmake -project
-#copy Comm_Msg.pro the DEPENDPATH, INCLUDEPATH, HEADERS, SOURCES to QComm_Msg.pro  
+#copy Comm_Msg.pro the DEPENDPATH, INCLUDEPATH, HEADERS, SOURCES to QPRO_NAME 
 if [ $2 = "-libshared" ]; then
     echo "shared library Makefile."
     if [ $1 = "-d" ]; then
-    	qmake QDEFS=x86_sharedlib -o Makefile QComm_Msg.pro
+    	qmake QDEFS=x86_sharedlib -o Makefile QPRO_NAME
     elif [ $1 = "-r" ]; then
- 	qmake QDEFS=arm_sharedlib -o Makefile QComm_Msg.pro
+ 	qmake QDEFS=arm_sharedlib -o Makefile QPRO_NAME
     fi
     time make
 fi
 #######################################################################################
 # liba_Comm_Msg.pro to Makefile
 #######################################################################################
-qmake -project
-#copy Comm_Msg.pro the DEPENDPATH, INCLUDEPATH, HEADERS, SOURCES to QComm_Msg.pro  
+#copy Comm_Msg.pro the DEPENDPATH, INCLUDEPATH, HEADERS, SOURCES to QPRO_NAME
 if [ $2 = "-libstatic" ]; then
     echo "static library Makefile."
     if [ $1 = "-d" ]; then
-    	qmake QDEFS=x86_staticlib -o Makefile QComm_Msg.pro
+    	qmake QDEFS=x86_staticlib -o Makefile QPRO_NAME
     elif [ $1 = "-r" ]; then
- 	qmake QDEFS=arm_staticlib -o Makefile QComm_Msg.pro
+ 	qmake QDEFS=arm_staticlib -o Makefile QPRO_NAME
     fi
     time make
 fi
